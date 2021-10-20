3df84d0c418bad1d40e6f5f302ca45971588ff63
koreaaerospaceU <zx811@naver.com>
2021-10-20 16:55:27 +0900

    feat: API 환경설정

---
+++ server/src/main/java/com/recruit/server/common/config/WebMvcConfiguration.java
@@ -0,0 +1,27 @@
+package com.recruit.server.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by Minky on 2021-06-09
+ */
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.HEAD.name()
+                );
+    }
+}
