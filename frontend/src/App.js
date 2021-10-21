140e248867ab2bad77486e4a96576b6b823cae89
koreaaerospaceU <zx811@naver.com>
2021-10-21 17:53:18 +0900

    feat: 로그인 페이지 제작 - 로그인 페이지 제작 - 로그인 대시보드 제작 - api util 제작 - cookie util 제작 - redirect util 제작

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import Alert from '@mui/material/Alert';
+import Collapse from '@mui/material/Collapse';
+import Box from '@mui/material/Box';
+import Login from './pages/login'
+// import Register from './pages/register'
+// import Storage from './pages/storage'
+// import Test from './pages/test'
+// import Schedule from './pages/schedule'
+// import ScheduleRegister from './pages/schedule-register'
+// import ScheduleWait from './pages/schedule-wait'
+import React from 'react'
 
-function App() {
+const App = () => {
+  const [errorAlert, setErrorAlert] = React.useState(false)
+  const [successAlert, setSuccessAlert] = React.useState(false)
+  const [warningAlert, setWarningAlert] = React.useState(false)
+  const [alertMessage, setAlertMessage] = React.useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Box sx={{ top: 0, left: 0, width: '100vw', position: 'absolute', zIndex: 100000 }}>
+        <Collapse in={errorAlert}>
+          <Alert variant="filled" severity="error">
+            {alertMessage}
+          </Alert>
+        </Collapse>
+      </Box>
+      <Box sx={{ top: 0, left: 0, width: '100vw', position: 'absolute', zIndex: 100000 }}>
+        <Collapse in={warningAlert}>
+          <Alert variant="filled" severity="warning">
+            {alertMessage}
+          </Alert>
+        </Collapse>
+      </Box>
+      <Box sx={{ top: 0, left: 0, width: '100vw', position: 'absolute', zIndex: 100000 }}>
+        <Collapse in={successAlert}>
+          <Alert variant="filled" severity="success">
+            {alertMessage}
+          </Alert>
+        </Collapse>
+      </Box>
+      <Switch>
+        {/* <Route exact path="/storage">
+                    <Storage
+                        setErrorAlert={setErrorAlert}
+                        setSuccessAlert={setSuccessAlert}
+                        setAlertMessage={setAlertMessage}
+                    />
+                </Route>
+                <Route exact path="/test">
+                    <Test />
+                </Route>
+                <Route exact path="/schedule">
+                    <Schedule
+                        setErrorAlert={setErrorAlert}
+                        setSuccessAlert={setSuccessAlert}
+                        setAlertMessage={setAlertMessage}
+                    />
+                </Route>
+                <Route exact path="/schedule/register">
+                    <ScheduleRegister
+                        setErrorAlert={setErrorAlert}
+                        setSuccessAlert={setSuccessAlert}
+                        setAlertMessage={setAlertMessage}
+                    />
+                </Route>
+                <Route exact path="/schedule/wait">
+                    <ScheduleWait
+                        setErrorAlert={setErrorAlert}
+                        setSuccessAlert={setSuccessAlert}
+                        setAlertMessage={setAlertMessage}
+                    />
+                </Route>
+                <Route exact path="/register">
+                    <Register />
+                </Route> */}
+        <Route exact path="/login">
+          <Login
+            setErrorAlert={setErrorAlert} setAlertMessage={setAlertMessage}
+          />
+        </Route>
+        <Redirect path="*" to="/login" />
+      </Switch>
+    </BrowserRouter >
   );
 }
 
---
+++ frontend/src/utils/api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+// const baseUrl = process.env.REACT_APP_API_URL
+const baseUrl = "http://localhost:8080"
+
+export const get = (middleUrl, payload) => {
+  return axios.get(baseUrl + middleUrl, payload)
+};
+
+export const post = (middleUrl, payload) => {
+  return axios.post(baseUrl + middleUrl, payload.body, {
+    headers: payload.headers
+  })
+};
+
+export const postNoHeader = (middleUrl, payload) => {
+  return axios.post(baseUrl + middleUrl, payload.body)
+};
+
+export const _delete = (middleUrl, payload) => {
+  return axios.delete(baseUrl + middleUrl, payload)
+};
